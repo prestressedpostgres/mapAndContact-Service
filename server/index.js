069479ff8194b8bb9069edcb0b8b392a04b1175b
Jared Bourget <jared.m.bourget@gmail.com>
2019-07-24 17:21:12 -0600

    pushing to new repo

--- server/index.js
+++ server/index.js
@@ -5,12 +5,12 @@ const bodyParser = require('body-parser');
 const db = require('../database/index.js');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// app.use(express.static(__dirname + '/../client/public'));
+app.use(express.static('../client/public'));
 
 
 
