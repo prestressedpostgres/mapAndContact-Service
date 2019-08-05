7618f89bb7b9a5bf377d5d20ff150286d4973543
Jared Bourget <jared.m.bourget@gmail.com>
2019-08-05 11:53:52 -0600

    Removed MongoDB Connections

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
-// const nr = require('newrelic');
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const database = require('../database/restaurant.js')
-const db = require('../database/index.js');
+
 const postgres = require('../database/postgreSQL.js')
 
 const app = express();
@@ -20,24 +19,6 @@ app.get('/pg/restaurant/:id', postgres.getRestaurantById)
 app.post('/pg/restaurants', postgres.createRestaurant)
 app.put('/pg/restaurant/:id', postgres.updateRestaurant)
 app.delete('/pg/restaurant/:id', postgres.deleteRestaurant)
-
-
-app.post('/add/restaurant', (req, res) => {
-  database.save(req.body);
-  res.send(console.log('data has been added to contacts'))
-})
-
-app.delete('/delete/restaurant', (req, res) => {
-  database.deleteEntry(req.body)
-  res.send(console.log('entry deleted from database'))
-})
-//take req.body, pass into .find function at db, take response,
-//send to props, and pass
-app.get('/api/contact/:name', function(req, res) {
-    db.findRestaurantData(req.params.name, (dbResponse)=>{
-      res.status(200).send(dbResponse)
-    })
-});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
